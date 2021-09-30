99d7c409e6198d5115e39da63b10dc6c622d4144
VedikaSinha <76154618+VedikaSinha@users.noreply.github.com>
2021-09-30 14:43:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+song = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+scoreleftWrist = 0;
+scorerightWrist = 0;
+
+function preload() {
+    song = loadSound("music.mp3");
+}
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
+}
+
+function modelLoaded() {
+    console.log("poseNet is initialized");
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        scoreleftWrist = results[0].pose.keypoints[9].score;
+        scorerightWrist = results[0].pose.keypoints[10].score;
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        rightWristY = results[0].pose.rightWrist.y;
+        rightWristX = results[0].pose.rightWrist.x;
+    }
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+    fill("red");
+    stroke("red");
+    if (scoreleftWrist > 0.2) {
+        circle(leftWristX, leftWristY, 20);
+        numberLeftWristY = Number(leftWristY);
+        removeDecimals = floor(numberLeftWristY);
+        volume = removeDecimals / 500;
+        document.getElementById("volume").innerHTML = "Volume = " + volume;
+        song.setVolume(volume);
+    }
+}
+
+function play() {
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
